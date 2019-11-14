c9cb55ac012e5291b73544de57efe7cc3787d0b1
Henry Lepa Malomalo <hlepa64@gmail.com>
2019-11-14 11:53:56 +0100

    thursday updates

--- app/controllers/accueil_controller.rb
+++ app/controllers/accueil_controller.rb
@@ -2,5 +2,6 @@ class AccueilController < ApplicationController
   def index
 	  @q = Etablissement.ransack(params[:q])
   	  @etablissement = @q.result(distinct: true)
+	  @articles = Article.all.limit(6).order("created_at desc")
   end
 end

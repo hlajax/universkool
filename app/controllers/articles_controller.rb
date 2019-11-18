43e63a1f0a497712db8421a9b5f8467b058f783c
Henry Lepa Malomalo <hlepa64@gmail.com>
2019-11-18 11:06:36 +0100

    monday updates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_administrateur!, ecxept:[:index, :show]
+  before_action :authenticate_administrateur!, except:[:index, :show]
   # GET /articles
   # GET /articles.json
   def index

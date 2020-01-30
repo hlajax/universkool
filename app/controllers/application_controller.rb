f18af17bed5cec401504235644957c1dda433262
Henry Lepa Malomalo <hlepa64@gmail.com>
2020-01-30 12:38:57 +0100

    3001 changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
   protected
   
-      def after_sign_in_path_for(administrateur)
-       dashboard_index_path
-     end
+      #def after_sign_in_path_for(administrateur)
+       #dashboard_index_path
+     #end
 	
 	
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :dashboard
   resources :messages
   resources :demandes
   resources :diplomes
